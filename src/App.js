9d7c10cad4ad06ea38b079035864e63b7080b338
Basil Meer <basilmeer@gmail.com>
2019-07-16 14:17:42 +0300

    Added the cv and resume, and update the portfolio JSON object

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import Contact from './Contact';
 import './App.css';
+import resume from './basilmeer-resume-june-2019.pdf';
+import cv from './basilmeer-cv-june-2019.pdf';
 // import picture from './basil-meer.jpg';
 
 class App extends Component {
@@ -22,8 +24,8 @@ class App extends Component {
               <div className="page-header">
                 <h1 className="display-4">Welcome</h1>
                 <p className="lead">I'm <span className="font-weight-bold">Basil Meer</span>, a <span className="font-weight-bold">Front-End Developer</span> and a <span className="font-weight-bold">UI/UX Designer</span>.</p>
-                <a href="#" className="btn btn-outline-primary mr-2">Resume</a>
-                <a href="#" className="btn btn-outline-secondary">CV</a>
+                <a target="_blank" href={ resume } className="btn btn-outline-primary mr-2">Resume</a>
+                <a target="_blank" href={ cv } className="btn btn-outline-secondary">CV</a>
               </div>
             </div>
           </div>
@@ -33,11 +35,21 @@ class App extends Component {
             <div className="row">
               <div className="col-lg-12">
                 <ul className="list-unstyled">
-                  <li className="float-right"><a href="#top">Back to top</a></li>
-                  <li>You can also find me here:</li>
-                  <li><a href="mailto:hello@basilmeer.com"><FontAwesomeIcon icon={ faEnvelope } /></a></li>
-                  <li><a href="https://www.linkedin.com/pk/basilmeer/"><FontAwesomeIcon icon={ faLinkedin } /></a></li>
-                  <li><a href="https://github.com/basilmeer/"><FontAwesomeIcon icon={ faGithub } /></a></li>
+                  <li className="float-right">
+                    <a href="#">Back to top</a>
+                  </li>
+                  <li>
+                    You can also find me here:
+                    </li>
+                  <li>
+                    <a href="mailto:hello@basilmeer.com"><FontAwesomeIcon icon={ faEnvelope } /></a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/pk/basilmeer/"><FontAwesomeIcon icon={ faLinkedin } /></a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/basilmeer/"><FontAwesomeIcon icon={ faGithub } /></a>
+                  </li>
                 </ul>
                 <p>Copyright 2019 &copy; <a href="https://www.basilmeer.com">Basil Meer</a>. All rights reserved.</p>
               </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,34 +3,34 @@ import PortfolioItem from './PortfolioItem';
 
 let portfolioList = [
   {
-    "title": "Title",
+    "title": "React and Redux IM",
     "image": "http://cdn.shibe.online/shibes/382fdb970e8ebf3c08063146dc8d67339c396918.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    "content": "Made a chat app built with React, Redux Saga and Websockets that runs purely on the client-side with no back-end."
   },
   {
-    "title": "Title",
-    "image": "http://cdn.shibe.online/shibes/b5cd5a2d04362c4d71357a58c5aa2f116cd3443e.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-    "title": "Title",
-    "image": "http://cdn.shibe.online/shibes/698ece2c56cddd05016ad9c1d3959204c288a0b3.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  {
-    "title": "Title",
+    "title": "OMDbrowser",
     "image": "http://cdn.shibe.online/shibes/0356e81bda039639f08713aec4a54b0fb611739e.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    "content": "Made React-based web app that uses the OMDb API to query and show movies, TV series and whatnot."
   },
   {
-    "title": "Title",
+    "title": "Nadius Solutions",
+    "image": "http://cdn.shibe.online/shibes/b5cd5a2d04362c4d71357a58c5aa2f116cd3443e.jpg",
+    "content": "Made the company website for a Dutch software house that provides various web-related services."
+  },
+  {
+    "title": "Veteranlogix",
+    "image": "http://cdn.shibe.online/shibes/698ece2c56cddd05016ad9c1d3959204c288a0b3.jpg",
+    "content": "Made the company website for a Pakistan-based software house that provides custom-tailored digital solutions."
+  },
+  {
+    "title": "ManilaForKids",
     "image": "http://cdn.shibe.online/shibes/515551501802df0bb043f791a395797a5e144f93.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    "content": "Improved the performance for a Phillipines-based resource for parents looking for activities and whatnot for their kids in Manila Metro."
   },
   {
-    "title": "Title",
+    "title": "React Quiz App",
     "image": "http://cdn.shibe.online/shibes/d6353cd7ab963d7dee0d9fe5bba08cb6e662e3f2.jpg",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    "content": "Made a React-based web app that uses a JSON object to display MCQs after which it parses the answers and provides the result."
   }
 ]
 
